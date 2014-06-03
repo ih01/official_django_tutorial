7ca299b6353bfe630334b89062393689afa89267
Iain <iain.alan.hendry@gmail.com>
2014-06-03 22:05:28 +0100

    tutorial 5 completed, tests written

---
+++ polls/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from polls.models import Choice, Poll
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+
+admin.site.register(Poll, PollAdmin)
